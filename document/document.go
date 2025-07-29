74e18a7008fb535b7537be3c59fd39d67f67e988
takaxis2 <axis1130@gmail.com>
2025-07-30 01:25:02 +0900

    Revert "nothing"
    This reverts commit a3c8c8feaf5bd6eb49a3c1e4143f913d3be066b9.

--- document/document.go
+++ document/document.go
@@ -11,11 +11,7 @@ type Document struct {
 }
 
 func NewDocument() *Document {
-	return &Document{}
-}
-
-func (d *Document) startup(ctx context.Context) {
-	d.ctx = ctx
+	return &Document{ctx: context.Background()}
 }
 
 // 폴더 선택 다이얼로그 함수 - 컨텍스트를 직접 받음
